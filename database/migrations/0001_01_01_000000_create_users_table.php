4606415ad26aca163eae4671a97389de4361c7a9
Duong <YuriVolkov1210@gmail.com>
2025-12-02 09:21:38 +0700

    feat: remove unused user attributes from the users migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -17,12 +16,6 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('avatar')->nullable();
-            $table->string('phone')->nullable();
-            $table->string('job_title')->nullable();
-            $table->string('company')->nullable();
-            $table->text('bio')->nullable();
-            $table->string('location')->nullable();
-            $table->boolean('is_visible')->default(true);
             $table->string('password');
             $table->text('two_factor_secret')->nullable();
             $table->text('two_factor_recovery_codes')->nullable();
